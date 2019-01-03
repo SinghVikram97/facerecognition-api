6cc42e16307da47861776c089000bc3fd9f74cb7
Vikram Singh Bedi <vikram.bedi97@gmail.com>
2019-01-03 18:28:13 +0530

    Added Api Config

--- server.js
+++ server.js
@@ -2,6 +2,19 @@ const express = require('express');
 
 const app = express();
 
+app.get('/', (req, res) => {
+  res.send("This is working");
+})
+
 app.listen(3000, () => {
   console.log("Server started on http://localhost:3000")
 })
+
+/*
+Api Config
+/ --> res=this is working
+/signin --> POST (because we are entering password so it should be post request) = sucess/fail
+/ register --> POST=user
+/profile/:userId --> GET=user
+/image --> PUT=user
+*/
