4a2089e3d760c8c37ae73eccbb3b2269dd92a9ef
Vikram Singh Bedi <vikram.bedi97@gmail.com>
2019-01-03 18:53:26 +0530

    Added signin route

--- server.js
+++ server.js
@@ -1,9 +1,45 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+app.use(bodyParser.json());
+
+const database = {
+  users: [{
+      id: '123',
+      name: 'John',
+      email: 'john@gmail.com',
+      password: 'cookies',
+      entries: 0,
+      joined: new Date()
+    },
+    {
+      id: '124',
+      name: 'Sally',
+      email: 'sally@gmail.com',
+      password: 'bannaas',
+      entries: 0,
+      joined: new Date()
+    }
+  ]
+}
+
 app.get('/', (req, res) => {
   res.send("This is working");
+})
+
+app.post('/signin', (req, res) => {
+  let email = req.body.email;
+  let password = req.body.password;
+  if (email === database.users[0].email && password === database.users[0].password) {
+    res.json('sucess');
+  } else {
+    res.status(400).json('Error loggin in');
+  }
 })
 
 app.listen(3000, () => {
