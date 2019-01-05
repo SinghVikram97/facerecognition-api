e196e5355ff9421b275e730ac420b8e2e9f0296a
Vikram Singh Bedi <vikram.bedi97@gmail.com>
2019-01-06 01:21:09 +0530

    Added port for heroku

--- server.js
+++ server.js
@@ -116,8 +116,8 @@ app.put("/image", (req, res) => {
     });
 });
 
-app.listen(3000, () => {
-  console.log("Server started on http://localhost:3000");
+app.listen(process.env.PORT || 3000, () => {
+  console.log(`Server started on ${process.env.PORT}`);
 });
 
 /*
